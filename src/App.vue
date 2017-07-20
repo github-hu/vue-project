e4047829e6c0b121c95c5e3885917b67bf507248
unknown <mamingze001>
2017-07-20 18:34:15 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view></router-view>
+    <ul>
+    	<li>lalala</li>
+    </ul>
   </div>
 </template>
 
@@ -11,13 +14,17 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+	@import './styles/reset.scss'; 
+	#app {
+	  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	  -webkit-font-smoothing: antialiased;
+	  -moz-osx-font-smoothing: grayscale;
+	  text-align: center;
+	  color: #2c3e50;
+	  margin-top: 60px;
+	}
+
+
+
 </style>
